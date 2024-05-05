3f28fa3a652613bddb712650610c2c4a21c005e1
ThePinkos <tomas.kubik2007@gmail.com>
2024-05-05 20:57:16 +0200

    Initial

---
+++ Kalendar/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Kalendar from '../views/KalendarView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/kalendar',
+      name: 'kalendar',
+      component: Kalendar
+    }
+  ]
+})
+
+export default router
